902d2cd45bf2b034245586b72e48248e9e045203
watarmeli <watar@ukr.net>
2021-05-05 13:11:51 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,18 @@
+import { error } from '@pnotify/core';
+
+
 const BASE_URL = 'https://restcountries.eu/rest/v2';
 
 function fetchCountry(countryName) {
   return fetch(`${BASE_URL}/name/${countryName}`).then(response => {
+    if (!response.ok) {
+      error({
+      text: 'Too many matches found. Please enter a more spesific query!',
+      delay: '2000',
+      maxTextHeight: null,
+      });
+      return;
+    }
     return response.json();
   });
 }
